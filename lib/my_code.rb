dd60aaff1e97e2811b4a980cf5f6f0d64bca4d81
Catherine Clause <cnclause123@gmail.com>
2019-08-24 22:52:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,8 @@
 # Your Code Here
 def map(array)
-  new [] 
-  counter = 0 
-  while counter < my_own_map.length do 
-  yield new.push() 
+ 
   
 end 
 
-map[array] do 
-  |n| "#{n} array[i] * 2 "
-end 
+
   
