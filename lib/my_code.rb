3463e91e18e2f978e798e78d350bf2045ecc9522
Catherine Clause <cnclause123@gmail.com>
2019-08-24 22:22:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,11 +3,12 @@ def my_own_map [array]
   new [] 
   counter = 0 
   while counter < my_own_map.length do 
-  puts new 
+  puts new.push() 
   yield 
   new 
 end 
 
 my_own_map[array] do 
-  |"double"| "#{double} "
+  |"double"| "#{double} source_array[i] * 2 "
+end 
   
